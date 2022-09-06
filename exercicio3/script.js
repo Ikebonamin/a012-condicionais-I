d4e0121c9685a9e3a4e58c8db35b4701e82919cf
Ikebonamin <bonamin.ike@gmail.com>
2022-09-06 15:56:55 -0300

    entrega exercícios da aula 12

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,37 @@
-let nacionalidade //= adicione o prompt aqui
+'strict';
+
+let nacionalidade = prompt('Escreva aqui sua nacionalidade');
+console.log(nacionalidade);
+nacionalidade = nacionalidade.toLowerCase();
+console.log(nacionalidade);
+
+if (nacionalidade == 'brasileira') {
+  console.log(`sua nacionalidade é ${nacionalidade}`);
+} else if (nacionalidade == 'argentina') {
+  console.log(`sua nacionalidade é ${nacionalidade}`);
+} else if (nacionalidade == 'uruguaia') {
+  console.log(`sua nacionalidade é ${nacionalidade}`);
+} else if (nacionalidade == 'chilena') {
+  console.log(`sua nacionalidade é ${nacionalidade}`);
+} else if (nacionalidade == 'colombiana') {
+  console.log(`sua nacionalidade é ${nacionalidade}`);
+} else {
+  console.log(`sua nacionalidade não é válida`);
+}
+
+// // usando  array //
+
+// let nacionalidade = prompt('Escreva aqui sua nacionalidade');
+// nacionalidade = nacionalidade.toLowerCase();
+
+// let nacionalidades = [
+//   'argentina',
+//   'colombiana',
+//   'uruguaia',
+//   'brasileira',
+//   'chilena',
+// ];
+// let resultado = nacionalidades.includes(nacionalidade)
+//   ? console.log('sim')
+//   : console.log('nao');
+// console.log(resultado);
